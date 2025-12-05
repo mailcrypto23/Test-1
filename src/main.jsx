a00874e07969d5c7f28ab6d0fde5b0daa20cc478
mailc <84892667+mailcrypto23@users.noreply.github.com>
2025-12-05 15:15:20 +0530

    Delete src directory
    test

--- src/main.jsx
+++
@@ -1,8 +0,0 @@
-import React from 'react'
-import { createRoot } from 'react-dom/client'
-import App from './App'
-import './styles.css'
-import './theme-dark.css'
-import './theme-light.css'
-
-createRoot(document.getElementById('root')).render(<App />)
